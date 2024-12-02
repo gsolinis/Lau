9f9d070691593a6ab2f6829df47ce3c34b62b0dd
GS <gsolinis@yahoo.com.ar>
2024-12-02 17:33:33 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,161 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_das-hauss_2024-12-02-12-55-48",
+      "name": "Panorama_DAS HAUSS_2024-12-02-12-55-48",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0812613218997491,
+        "pitch": 0,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.404869337899827,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-panorama_das-hauss_2024-12-02-14-52-53"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6596241925633972,
+          "pitch": 0.47570203697303626,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.34927256121148,
+          "pitch": 0.003105441337856618,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7110305062403057,
+          "pitch": -0.10808564937232745,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.22553113800703,
+          "pitch": 0.42496398815402436,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.051551813254892664,
+          "pitch": 0.5686574299216609,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0140191585438867,
+          "pitch": 0.18572353953896403,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama_das-hauss_2024-12-02-14-52-53",
+      "name": "Panorama_DAS HAUSS_2024-12-02-14-52-53",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.2131381293874703,
+        "pitch": 0.2590157257401966,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.127450322222959,
+          "pitch": 0.10080474912722792,
+          "rotation": 0,
+          "target": "0-panorama_das-hauss_2024-12-02-12-55-48"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7635732149137997,
+          "pitch": 0.482390557730362,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.073187866825938,
+          "pitch": 0.3092949768155613,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2275392642861878,
+          "pitch": 0.07051131678711187,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.454851300879902,
+          "pitch": -0.11208511676947808,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0436461141799747,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9914352611759636,
+          "pitch": -0.171941140824309,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "DAS HAUSS",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
